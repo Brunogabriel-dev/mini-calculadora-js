c7add680375e89d784047585bcf9c04c34cbccef
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-29 22:19:20 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ Entre 25 e 29,99 Acima do peso
 var peso;
 var altura;
 var imc;
-var resultados;
+var resultado;
 
 function calcular(event){
   event.preventDefault();
 
   peso = document.getElementById('peso').value;
   altura = document.getElementById('altura').value;
+
+  imc = peso / (altura * altura);
+
+  if(imc <17){
+    resultado = document.getElementById('resultado');
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Cuidado você está muito abaixo do peso!'
+  }
+
+
 }
