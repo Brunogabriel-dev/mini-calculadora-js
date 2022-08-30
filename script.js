75971d549841b67c0d833c71483e434b07e71dc2
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-29 22:44:12 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ function calcular(event){
 
   if(imc <17){
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Cuidado você está muito abaixo do peso!'
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'
   }else if(imc > 17 && imc <= 18.49){
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Você está Abaixo do peso!';
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está Abaixo do peso!';
   }else if(imc > 18.5 && imc <= 24.99){
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Você está no peso ideal!';
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está no peso ideal!';
   }else if(imc > 25 && imc <= 29.99){
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Você está acima do peso!';
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está acima do peso!';
   }else if(imc >= 30){
     resultado = document.getElementById('resultado');
-    resultado.innerHTML = '<br/> Seu resultado foi:' + imc + '<br/> Cuidado Obesidade!';
+    resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado Obesidade!';
   }
 
 
