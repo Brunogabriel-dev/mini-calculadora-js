e6012c6531537a77b005d248dd0934eb9f3cdb10
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-08-29 22:47:23 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -21,20 +21,17 @@ function calcular(event){
 
   imc = peso / (altura * altura);
 
+  resultado = document.getElementById('resultado');
+
   if(imc <17){
-    resultado = document.getElementById('resultado');
     resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado você está muito abaixo do peso!'
   }else if(imc > 17 && imc <= 18.49){
-    resultado = document.getElementById('resultado');
     resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está Abaixo do peso!';
   }else if(imc > 18.5 && imc <= 24.99){
-    resultado = document.getElementById('resultado');
     resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está no peso ideal!';
   }else if(imc > 25 && imc <= 29.99){
-    resultado = document.getElementById('resultado');
     resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Você está acima do peso!';
   }else if(imc >= 30){
-    resultado = document.getElementById('resultado');
     resultado.innerHTML = '<br/> Seu resultado foi:' + imc.toFixed(2) + '<br/> Cuidado Obesidade!';
   }
 
